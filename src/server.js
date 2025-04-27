9e1b95a2748d4b1ce0b703a87d55d6f66fa404e8
luisaMarin18 <136044475+luisaMarin18@users.noreply.github.com>
2025-04-26 20:54:36 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import router from './routes/customers.routes';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.NODE_PORT || 5000;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rutas
+app.use('/customers', router);
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
 
