72047cd851501955544ea3c567a34cad87360b07
luisaMarin18 <136044475+luisaMarin18@users.noreply.github.com>
2025-04-26 21:11:14 -0500

    Update createCustomerDB.sql

--- src/database/createCustomerDB.sql
+++ src/database/createCustomerDB.sql
@@ -12,3 +12,25 @@ CREATE TABLE IF NOT EXISTS customers (
     email VARCHAR(100) UNIQUE NOT NULL, -- Definir la columna 'email' para almacenar el correo electrónico, debe ser único y no nula
     age INT DEFAULT 0 -- Definir la columna 'age' para almacenar la edad, con un valor predeterminado de 0
 );
+
+/* 
+* Esta consulta verifica si existe un esquema llamado test_db, y lo crea si no existe
+*/
+CREATE SCHEMA IF NOT EXISTS test_db;
+
+/*
+* Esta consulta verifica si existe una tabla llamada 'customers', y si existe, la elimina
+*/
+DROP TABLE IF EXISTS customers;
+
+/*
+* Si la tabla no existe, esta consulta la crea con los campos que especificamos
+*/
+CREATE TABLE IF NOT EXISTS customers (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  -- ID del cliente, clave primaria y autoincremental
+    first_name VARCHAR(50) NOT NULL,             -- Nombre del cliente
+    last_name VARCHAR(50) NOT NULL,              -- Apellido del cliente
+    email VARCHAR(100) UNIQUE NOT NULL,          -- Email del cliente, debe ser único
+    age INT DEFAULT 0                            -- Edad del cliente, por defecto es 0
+);
+
